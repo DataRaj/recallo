e2d21765bbbb8ad1c09d3c3580a96bcf8527aeb7
Dattaraj Dhadas <dhadas808@yahoo.com>
2026-06-10 17:29:45 +0530

    Updates: A: 2 M: 1 - feature:Added the authorization middleware for handling the flow of login/logout

---
+++ internals/middleware/authentication.go
@@ -0,0 +1,58 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"gotel/internals/utils"
+)
+
+const (
+	CtxUserID          string = "userId"
+	CtxUserDisplayName string = "name"
+	CtxPlatform        string = "X-Platform"
+	CtxAuthorization   string = "Authorization"
+	PlatformWeb               = "web"
+	PlatformMobile            = "mobile"
+)
+
+func AuthenticateMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := strings.TrimSpace(r.Header.Get(CtxAuthorization))
+		if authHeader == "" || !strings.HasPrefix(strings.ToLower(authHeader), "bearer ") {
+			utils.JSON(w, http.StatusUnauthorized, false, "Unauthorized", nil)
+			return
+		}
+
+		accessToken := strings.TrimSpace(authHeader[7:])
+
+		userId, name, tokenPlatform, err := utils.VerifyJWT(accessToken)
+		if err != nil {
+			utils.JSON(w, http.StatusUnauthorized, false, "Unauthorized", nil)
+			return
+		}
+
+		platform := strings.ToLower(strings.TrimSpace(r.Header.Get(CtxPlatform)))
+		if platform == "" {
+			platform = tokenPlatform
+		}
+
+		if platform != PlatformWeb && platform != PlatformMobile {
+			utils.JSON(w, http.StatusBadRequest, false, "Invalid platform", nil)
+			return
+		}
+
+		if tokenPlatform != platform {
+			utils.JSON(w, http.StatusUnauthorized, false, "Unauthorized", nil)
+			return
+		}
+
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, CtxUserID, userId)
+		ctx = context.WithValue(ctx, CtxUserDisplayName, name)
+		ctx = context.WithValue(ctx, CtxPlatform, platform)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
